7c3f00070e85cc033c4c387969a1a4d53e8fb3fe
Jer <jcarlson_33@hotmail.com>
2017-01-21 02:03:34 -0600

    assignment-13-DOM_play/pause2

--- scripts/album.js
+++ scripts/album.js
@@ -75,12 +75,21 @@ var setCurrentAlbum = function(album) {
 };
 
 var findParentByClassName = function(element, targetClass) {
-    if (element) {
+    if (element.parentElement != null) {
+        
         var currentParent = element.parentElement;
-        while(currentParent.className != targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
+        
+        if(currentParent.className !== null){
+            
+            while(currentParent.className != targetClass) {
+                currentParent = currentParent.parentElement;
+            }
+            return currentParent;
+        }else{
+            alert("No parent found with that class name");
         }
-        return currentParent;
+    }else {
+        alert("No parent found");
     }
 };
 
